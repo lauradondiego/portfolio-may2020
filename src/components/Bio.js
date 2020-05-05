ab563472c1b840d5ee29a59dbfaaa612ed0bc2d8
laura <lauradondiego@gmail.com>
2020-05-05 16:28:27 -0400

    added bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,10 +1,32 @@
 import React, { Component } from "react"
+import { Label, Image } from "semantic-ui-react"
 
 export default class Bio extends Component {
     render() {
         return (
             <>
                 <h1>BIO</h1>
+                <p> ** add upcoming FEAR CITY, business owner ON THE LEASH etc **
+                In 2018 I embarked on an exciting journey to New York City, and took
+            Intro to Python head on <Label>print("Hello World")</Label> It
+            has been quite the adventure since.
+                    My curiousty led me to try this new thing that I had only heard about, my spontaneous side urged me to quit my 9-5 and pursue it
+                    full time, and my dedication is what made me never look back.
+
+                    When I'm not coding, I enjoy spending time at home with my pets, reading mystery and horror novels, seeking out new
+                    restaurants, making memories with friends, and working on hobbies and all types of projects from coding to crafting.
+
+                    I've traveled around the world and am fascinated by culture, food,
+                    and technology. I lived in Africa at a wildlife rehabilitation
+                    center, making new friends, taking on leadership roles, and most
+                    importantly, saving the lives of Africa's wildlife and fighting for
+
+                    I am passionate and not afraid to take risks, yet am level headed
+                    and can make quick decisions under pressure.
+
+                    I am career focused and eager to learn new technologies and
+                    languages. The most rewarding feeling is seeing something I have
+            created come to life!</p>
             </>
         )
     }
