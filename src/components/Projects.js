aad16f73e37ea68d4462bc77473f61770409881c
laura <lauradondiego@gmail.com>
2020-05-05 19:12:35 -0400

    adding links to projects to open on new page not fully functioning yet

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,8 +7,6 @@ import bf_demo from "./assets/bf_demo.png"
 import ng_logo from "./assets/ng_logo.png"
 import ng_demo from "./assets/ng_demo.png"
 
-import NetGiver from "./NetGiver"
-
 import "../css/Projects.css"
 
 export default class Projects extends Component {
@@ -23,7 +21,8 @@ export default class Projects extends Component {
                         <figcaption>
                             <h2>Net <span>Giver</span></h2>
                             <p>simplify all past, present, &amp; upcoming work orders, and assigned projects, in one convenient and organized place.</p>
-                            <a href="/NetGiver">View more</a>
+                            <Link to="/netgiver">
+                            </Link>
                         </figcaption>
                     </figure>
 
@@ -32,8 +31,9 @@ export default class Projects extends Component {
                         <figcaption>
                             <h2>Better <span>Professor</span></h2>
                             <p>facilitate message retrieval, keep track of important due dates, project statuses, and letters of recommendation. </p>
-                            <Link to="/netgiver">
+                            <Link to="/betterprofessor">
                             </Link>
+
                         </figcaption>
                     </figure>
                 </div>
