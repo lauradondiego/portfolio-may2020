2e3eefa574e981c31579aa6a88a2f723269f3c2e
laura <lauradondiego@gmail.com>
2020-05-06 18:00:59 -0400

    fixed wording in bio

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,8 +5,8 @@ export default class Welcome extends Component {
         return (
             <>
                 <h1>Welcome! </h1>
-                <h1> I am an experienced New Jersey based Full-Stack Web Developer with a passion for creating and design. I’m available for full-time opportunities and freelance work. I specialize in building JS based React Apps as well as Python, design, &amp; full-stack sites.
-                Contact here link
+                <h1> I am an experienced Full-Stack Web Developer with a passion for creating and design, based in New Jersey. I’m available for full-time opportunities and freelance work, &amp; am open to relocating. I specialize in building JS based React Apps as well as Python, design, &amp; full-stack sites.
+                Contact Me Now link
 </h1>
             </>
         )
