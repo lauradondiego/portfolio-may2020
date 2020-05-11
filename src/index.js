3063a364ec3130f344b19448f56d09b29d8b7267
laura <lauradondiego@gmail.com>
2020-05-11 15:30:18 -0400

    got react-mdl to work see slack for how i debugged and made imports work

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'semantic-ui-less/semantic.less'
+import 'semantic-ui-less/semantic.less';
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
 
 import { BrowserRouter } from "react-router-dom";
 
--- src/navbar/NavBar.js
+++ src/navbar/NavBar.js
@@ -1,22 +1,47 @@
 import React, { Component } from "react";
-import { NavLink } from "react-router-dom";
+// import { NavLink } from "react-router-dom";
+import { Layout, Header, Textfield, Drawer, Navigation } from 'react-mdl';
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
 
-import "../css/NavBar.css"
+// import "../css/NavBar.css"
 
 export default class NavBar extends Component {
     render() {
         return (
             <>
-                <nav>
+                <h1>hi</h1>
+                {/* style={{ height: '300px', position: 'relative' }} */}
+                <Layout fixedHeader fixedDrawer>
+                    <Header title="Title">
+                        <Textfield
+                            value=""
+                            onChange={() => { }}
+                            label="Search"
+                            expandable
+                            expandableIcon="search"
+                        />
+                    </Header>
+                    <Drawer title="Title">
+                        <Navigation>
+                            <a href="/welcome">Link</a>
+                            <a href="/projects">Link</a>
+                            <a href="/bio">Link</a>
+                            <a href="/contact">Link</a>
+                        </Navigation>
+                    </Drawer>
+                </Layout>
+                {/* </div> */}
+            </>
+        );
+    }
+}
+
+/* <nav>
                     <ul>
                         <NavLink to="/welcome">Welcome</NavLink>
                         <NavLink to="/projects">Projects</NavLink>
                         <NavLink to="/bio">Bio</NavLink>
                         <NavLink to="/contact">Contact</NavLink>
                     </ul>
-                </nav>
-            </>
-        );
-    }
-}
-
+                </nav> */
