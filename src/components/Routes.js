1030a052773ef4c3529b475620cda069b89e405f
laura <lauradondiego@gmail.com>
2020-05-13 12:58:46 -0400

    changed nav bar layout from react-mdl designs

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -3,6 +3,7 @@ import { Route, Switch, BrowserRouter } from "react-router-dom"
 import App from "../App"
 import Welcome from "./Welcome"
 import Projects from "./Projects"
+import Bio from "./Bio"
 import Contact from "./Contact"
 
 function Routes() {
@@ -11,10 +12,8 @@ function Routes() {
             <Switch>
                 <Route exact path="/" component={Welcome} />
                 <Route path="/projects" component={Projects} />
+                <Route path="/bio" component={Bio} />
                 <Route path="/contact" component={Contact} />
-
-                {/* {/* <Route path="/projects" render={props => <Projects {...props} />} /> */}
-                {/* <Route path="/upcoming" render={props => <Upcoming {...props} />} /> */}
             </Switch>
         </div>
     );
--- src/navbar/NavBar.js
+++ src/navbar/NavBar.js
@@ -11,26 +11,55 @@ export default class NavBar extends Component {
     render() {
         return (
             <nav>
+
+                <div style={{ height: '300px', position: 'relative' }}>
+                    <Layout style={{ background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover' }}>
+                        <Header transparent title="Title" style={{ color: 'white' }}>
+                            <Navigation>
+                                <NavLink to="/">Welcome!</NavLink>
+                                <NavLink to="/projects" activeStyle={{
+                                    background: 'red',
+                                    color: 'white'
+                                }}>Projects</NavLink>
+                                <NavLink to="/bio">Bio</NavLink>
+                                <NavLink to="/contact">Contact</NavLink>
+                            </Navigation>
+                        </Header>
+                        <Drawer title="Title">
+                            <Navigation>
+                                <NavLink to="/">Welcome!</NavLink>
+                                <NavLink to="/projects" activeStyle={{
+                                    background: 'red',
+                                    color: 'white'
+                                }}>Projects</NavLink>
+                                <NavLink to="/bio">Bio</NavLink>
+                                <NavLink to="/contact">Contact</NavLink>
+                            </Navigation>
+                        </Drawer>
+                        <Content />
+                    </Layout>
+                </div>
+
+
+
                 {/* <div style={{ height: '300px', position: 'relative' }} > */}
-                <Layout fixedHeader fixedDrawer>
+                {/* <Layout fixedHeader fixedDrawer>
                     <Header title="Welcome!">
 
                     </Header>
                     <Drawer title="Laura Don Diego">
                         <Navigation>
-                            <NavLink to="/">Welcome!</NavLink>
+                            ```    <NavLink to="/">Welcome!</NavLink>
                             <NavLink to="/projects" activeStyle={{
                                 background: 'red',
                                 color: 'white'
                             }}>Projects</NavLink>
                             <NavLink to="/bio">Bio</NavLink>
-                            <NavLink to="/contact">Contact</NavLink>
+                            <NavLink to="/contact">Contact</NavLink>```
                         </Navigation>
-                    </Drawer>
-                    {/* <Content>
-                        <Routes />
-                    </Content> */}
-                </Layout>
+                    </Drawer> */}
+
+                {/* </Layout> */}
                 {/* </div> */}
             </nav>
         );
