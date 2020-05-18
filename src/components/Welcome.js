e21327c80e951114af8d7fa021b4460e53fff0b3
laura <lauradondiego@gmail.com>
2020-05-18 18:19:14 -0400

    added welcomet container and welcomet.css file with styles for that container

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
 
+import "../css/welcome.css"
+
 export default class Welcome extends Component {
     render() {
         return (
-            <>
+            <div className="welcome-container">
                 <h1>Welcome! </h1>
                 <h1> I am an experienced Full-Stack Web Developer with a passion for creating and design, based in New Jersey. I’m available for full-time opportunities and freelance work, &amp; am open to relocating. I specialize in building JS based React Apps as well as Python, design, &amp; full-stack sites.
                 Contact Me Now link
 </h1>
-            </>
+            </div>
         )
     }
 
