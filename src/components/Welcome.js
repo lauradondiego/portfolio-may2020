d95ecffafc53f8ff15d7a9cb85eec3df3b37eb81
laura <lauradondiego@gmail.com>
2020-05-19 12:38:50 -0400

    adding image on welcome file to stick to the top of the page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,11 +4,13 @@ import Projects from "../components/Projects"
 import Bio from "../components/Bio"
 import Contact from "../components/Contact"
 import "../css/welcome.css"
+import { welcome_img } from "../components/assets/topbar_me.jpg"
 
 export default class Welcome extends Component {
     render() {
         return (
             <div className="welcome-container">
+                <img src={welcome_img} alt="image"></img>
                 <h1>Welcome! </h1>
                 <h1> I am an experienced Full-Stack Web Developer with a passion for creating and design, based in New Jersey. I’m available for full-time opportunities and freelance work, &amp; am open to relocating. I specialize in building JS based React Apps as well as Python, design, &amp; full-stack sites.
                 Contact Me Now link</h1>
