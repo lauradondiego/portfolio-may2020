aaf0fa5a769c6050e66d76971c7253f71c8f7acc
laura <lauradondiego@gmail.com>
2020-05-21 17:13:15 -0400

    need to use hooks for this so refactoring in progress

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,17 +1,34 @@
 import React, { Component } from "react"
 import { Label, Image } from "semantic-ui-react"
 
+import ng_logo from "../components/assets/ng_logo.png"
 import "../css/bio.css"
 
 export default class Bio extends Component {
-    render() {
-        return (
-            <div className="bio-container">
+    constructor(props) {
+        super(props);
+        this.plusSlides = this.plusSlides.bind(this);
+    }
 
-                <h1>BIO</h1>
-                <h2>ADD RESUME LINK</h2>
-                <h2>
-                    In 2018 I embarked on an exciting journey to New York City, and took
+    // slideIndex = 1
+    // showSlides(slideIndex);
+
+    plusSlides(n) {
+        var slideIndex = 1;
+        showSlides(slideIndex);
+        showSlides(slideIndex += n)
+        console.log('click happened');
+    }
+    render() {
+
+        return (
+            <>
+                <div className="bio-container">
+
+                    <h1>BIO</h1>
+                    <h2>ADD RESUME LINK</h2>
+                    <h2>
+                        In 2018 I embarked on an exciting journey to New York City, and took
             Intro to Python head on <Label>print("Hello World")</Label> It
             has been quite the adventure since.
                     My curiousty led me to try this new thing that I had only heard about, my spontaneous side urged me to quit my 9-5 and pursue it
@@ -38,61 +55,65 @@ export default class Bio extends Component {
                     I am career focused and eager to learn new technologies and
                     languages. As a successful entrepreneuer and well balanced individual, I am confident I can take on any challenge in the tech world
             and am excited to create.</h2>
-                <div className="gallery-container">
-                    <div className="mySlides">
-                        <img src="url" alt="pic1" />
-                    </div>
-                    <div className="mySlides">
-                        <img src="url" alt="pic2" />
-                    </div>
-                    <div className="mySlides">
-                        <img src="url" alt="pic3" />
-                    </div>
-                    <div className="mySlides">
-                        <img src="url" alt="pic4" />
-                    </div>
-                    <div className="mySlides">
-                        <img src="url" alt="pic5" />
-                    </div>
-
-                    <a className="prev" onClick="plusSlides(-1)"></a>
-                    <a className="next" onClick="plusSlides(1)"></a>
-
-                    <div className="caption-container">
-                        <p id="caption"></p>
-                    </div>
-
-                    <div className="row">
-                        <div className="column">
-                            <img className="demo cursor" src="url" onClick="currentSlide(1)" alt="pic1">
-
-                            </img>
+                    <div className="gallery-container">
+                        <div className="mySlides">
+                            <img src={ng_logo} alt="img01" />
                         </div>
-                        <div className="column">
-                            <img className="demo cursor" src="url" onClick="currentSlide(2)" alt="pic2">
-
-                            </img>
+                        <div className="mySlides">
+                            <img src={ng_logo} alt="pic2" />
                         </div>
-                        <div className="column">
-                            <img className="demo cursor" src="url" onClick="currentSlide(3)" alt="pic3">
-
-                            </img>
+                        <div className="mySlides">
+                            <img src={ng_logo} alt="pic3" />
                         </div>
-                        <div className="column">
-                            <img className="demo cursor" src="url" onClick="currentSlide(4)" alt="pic4">
-
-                            </img>
+                        <div className="mySlides">
+                            <img src={ng_logo} alt="pic4" />
                         </div>
-                        <div className="column">
-                            <img className="demo cursor" src="url" onClick="currentSlide(5)" alt="pic5">
-
-                            </img>
+                        <div className="mySlides">
+                            <img src={ng_logo} alt="pic5" />
                         </div>
 
+                        {/* <button className="prev" onClick={plusSlides - 1} />
+        <button className="next" onClick={plusSlides + 1} />  */}
+                        <button onClick={this.plusSlides}>previous</button>
+                        <button onClick={this.plusSlides}>next</button>
+
+                        <div className="caption-container">
+                            <p id="caption"></p>
+                        </div>
+
+                        <div className="row">
+                            <div className="column">
+                                <img className="demo cursor" src={ng_logo} onClick={currentSlide(1)} alt="pic1">
+
+                                </img>
+                            </div>
+                            <div className="column">
+                                <img className="demo cursor" src={ng_logo} onClick={currentSlide(2)} alt="pic2">
+
+                                </img>
+                            </div>
+                            <div className="column">
+                                <img className="demo cursor" src={ng_logo} onClick="currentSlide(3)" alt="pic3">
+
+                                </img>
+                            </div>
+                            <div className="column">
+                                <img className="demo cursor" src={ng_logo} onClick="currentSlide(4)" alt="pic4">
+
+                                </img>
+                            </div>
+                            <div className="column">
+                                <img className="demo cursor" src={ng_logo} onClick="currentSlide(5)" alt="pic5">
+
+                                </img>
+                            </div>
+
+                        </div>
                     </div>
+
                 </div>
-
-            </div>
+            </>
         )
     }
 }
+
