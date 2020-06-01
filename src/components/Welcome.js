fc0d94ad292d3a12b2626cc7aeef614f5e7e6a0b
laura <lauradondiego@gmail.com>
2020-06-01 16:05:11 -0400

    changed the say hello to email into a button that goes directly to the contact component with a pop up smiley instead

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -8,11 +8,13 @@ import { welcome_img } from "../components/assets/topbar_me.jpg"
 
 import { Link } from "react-router-dom"
 import BetterProfessor from "./BetterProfessor"
-import { Image, Reveal, Grid } from 'semantic-ui-react'
+import { Divider } from 'semantic-ui-react'
 import bf_logo from "./assets/bf_logo.png"
 import bf_demo from "./assets/bf_demo.png"
 import ng_logo from "./assets/ng_logo.png"
 import ng_demo from "./assets/ng_demo.png"
+
+import { MdTagFaces } from "react-icons/md"
 
 // export default class Welcome extends Component {
 
@@ -23,54 +25,58 @@ function Welcome() {
 
     return (
         <div className="welcome-container">
-            <img className="sticky" src={welcome_img} alt="Avatar" />
+            {/* <img className="sticky" src={welcome_img} alt="Avatar" /> */}
             <div className="headers">  Hello! </div>
-           
-            <span> I am an experienced Web Developer with a passion for creating and design, based in NJ. 
-           I’m available for <span class="theme-color">full-time opportunities and freelance work. </span> 
+
+            <span> I am an experienced Web Developer with a passion for creating and design, based in NJ.
+           I’m available for<span class="theme-color">full-time opportunities and freelance work. </span>
             I specialize in building JS based React Apps as well as Python, design, &amp; full-stack sites.</span>
             <span className="contact-now"
                 onMouseEnter={() => setIsShown(true)}
                 onMouseLeave={() => setIsShown(false)}>
-                Contact ME NOW!
-            
-            {isShown && (
-                <a href="mailto:lauradondiego@gmail.com">
-                    lauradondiego@gmail.com
-                </a>)}
+                <button> <Link to="/contact"> Say hello!</Link></button>
+
+                {isShown && (
+                    // <a href="mailto:lauradondiego@gmail.com">
+                    //     lauradondiego@gmail.com
+                    // </a>
+                    <MdTagFaces />
+                )}
             </span>
+
+
+            {/* <div style={{ background:url("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSYLwsQH948ZDf7-HsACdOEuo5Je5HrTDHLY7qZf4XFuPtawtiK&usqp=CAU")}}></div> */}
+
             <div className="projects-container-welcome">
-            <div className="grid">
+                <div className="grid">
                     <div className="headers">  Selected Works </div>
-
-                <figure class="effect-sadie">
-                    <img src={ng_logo} alt="img01" />
-                    <figcaption>
-                        <h2>Net <span>Giver</span></h2>
-                        <p>simplify all past, present, &amp; upcoming work orders, and assigned projects, in one convenient and organized place.</p>
-                        <Link to="/netgiver">
+                    <figure class="effect-sadie">
+                        <img src={ng_logo} alt="img01" />
+                        <figcaption>
+                            <h2>Net <span>Giver</span></h2>
+                            <p>simplify all past, present, &amp; upcoming work orders, and assigned projects, in one convenient and organized place.</p>
+                            <Link to="/netgiver">
                             </Link>
-                    </figcaption>
-                </figure>
+                        </figcaption>
+                    </figure>
 
-                <figure class="effect-sadie">
-                    <img src={bf_logo} alt="img01" />
-                    <figcaption>
-                        <h2>Better <span>Professor</span></h2>
-                        <p>facilitate message retrieval, keep track of important due dates, project statuses, and letters of recommendation. </p>
-                        <Link to="/betterprofessor">
-                        </Link>
+                    <figure class="effect-sadie">
+                        <img src={bf_logo} alt="img01" />
+                        <figcaption>
+                            <h2>Better <span>Professor</span></h2>
+                            <p>facilitate message retrieval, keep track of important due dates, project statuses, and letters of recommendation. </p>
+                            <Link to="/betterprofessor">
+                            </Link>
 
-                    </figcaption>
-                </figure>
+                        </figcaption>
+                    </figure>
+                </div>
             </div>
-            </div>
-
             <div className="bio-container-welcome">
                 <div className="headers"> About Me </div>
                 <span> Learn more about me here link</span>
             </div>
-           
+
             <div className="contact-container-welcome">
                 <div className="headers"> Contact Me </div>
                 <p>              <a href="mailto:lauradondiego@gmail.com">
