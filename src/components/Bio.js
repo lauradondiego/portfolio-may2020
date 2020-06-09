e4e3fd0fd33563f30ddc4db56962615c77195d4a
laura <lauradondiego@gmail.com>
2020-06-09 16:57:57 -0400

    bio skeleton is added to component needs some tweaking but could be sent out

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -10,38 +10,45 @@ export default class Bio extends Component {
             <>
                 <div className="bio-container">
 
-                    <h1>BIO</h1>
-                    <h2>ADD RESUME LINK</h2>
+                    <div className="headers">About Me</div>
+                    <a href="https://resume.creddle.io/resume/ibr75r9gn12">Resume</a>
+                    {/* maybe make the resume load on a new page */}
                     {/* some words need to be bold or colored to stand out! */}
-                    <h2>
+                    <span>
                         In 2018 I embarked on an exciting journey to New York City, and took
             Intro to Python head on <Label>print("Hello World")</Label> It
             has been quite the adventure since.
                     My curiousty led me to try this new thing that I had only heard about, my spontaneous side urged me to quit my 9-5 and pursue it
                     full time, and my dedication is what made me never look back.
-
-                    When I'm not coding, I am emersed in my streetwear brand, learning new design tools, social media marketing,
-                    and brainstorming new sketch ideas for upcoming launches. The most rewarding feeling is seeing something I have
-            created come to life!
-
-                    I enjoy volunteering at my local SPCA animal shelter, as well as spending time with my own pets.
-                    I like reading mystery and horror novels, listening to podcasts about outter space, and watching Earth documentaries.
-
-                    My typical weekends include seeking out new
-                    restaurants, competitive game nights with friends, and traveling whenever I can.
-
-                    Throughout my extensive travels, I have become fascinated by culture, food,
-                    and technology. A defining moment in my life was when I lived in Africa at a wildlife rehabilitation
-                    center. I overcame challenges, made lifelong friends, took on leadership roles, and most
-                    importantly, aided in saving the lives of Africa's wildlife.
-
-                    I am passionate and not afraid to take risks, yet am level headed
-                    and can make quick decisions under pressure.
-
-                    I am career focused and eager to learn new technologies and
-                    languages. As a successful entrepreneuer and well balanced individual, I am confident I can take on any challenge in the tech world
-            and am excited to create.</h2>
-                    <div className="gallery-container">
+</span>
+                    <span>
+                        When I'm not coding, I am emersed in my streetwear brand, learning new design tools, social media marketing,
+                        and brainstorming new sketch ideas for upcoming launches. The most rewarding feeling is seeing something I have
+                        created come to life!
+</span>
+                    <span>
+                        I enjoy volunteering at my local SPCA animal shelter, as well as spending time with my own pets.
+                        I like reading mystery and horror novels, listening to podcasts about outter space, and watching Earth documentaries.
+            </span>
+                    <span>
+                        My typical weekends include seeking out new
+                        restaurants, competitive game nights with friends, and traveling whenever I can.
+</span >
+                    <span>
+                        Throughout my extensive travels, I have become fascinated by culture, food,
+                        and technology. A defining moment in my life was when I lived in Africa at a wildlife rehabilitation
+                        center. I overcame challenges, made lifelong friends, took on leadership roles, and most
+                        importantly, aided in saving the lives of Africa's wildlife.
+</span >
+                    <span>
+                        I am passionate and not afraid to take risks, yet am level headed
+                        and can make quick decisions under pressure.
+</span >
+                    <span>
+                        I am career focused and eager to learn new technologies and
+                        languages. As a successful entrepreneuer and well balanced individual, I am confident I can take on any challenge in the tech world
+            and am excited to create.</span>
+                    {/* <div className="gallery-container">
                         <div className="mySlides">
                             <img src={ng_logo} alt="img01" />
                         </div>
@@ -56,11 +63,11 @@ export default class Bio extends Component {
                         </div>
                         <div className="mySlides">
                             <img src={ng_logo} alt="pic5" />
-                        </div>
+                        </div> */}
 
-                        {/* <button className="prev" onClick={plusSlides - 1} />
+                    {/* <button className="prev" onClick={plusSlides - 1} />
         <button className="next" onClick={plusSlides + 1} />  */}
-                        <button onClick={this.plusSlides}>previous</button>
+                    {/* <button onClick={this.plusSlides}>previous</button>
                         <button onClick={this.plusSlides}>next</button>
 
                         <div className="caption-container">
@@ -92,10 +99,10 @@ export default class Bio extends Component {
                                 <img className="demo cursor" src={ng_logo} onClick="currentSlide(5)" alt="pic5">
 
                                 </img>
-                            </div>
+                            </div> */}
 
-                        </div>
-                    </div>
+                    {/* </div> */}
+                    {/* </div> */}
 
                 </div>
             </>
