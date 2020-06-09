ff614ea313b9a5a4652eba31687e292e362e2bff
laura <lauradondiego@gmail.com>
2020-06-09 16:59:56 -0400

    contact component good enough to send out

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,21 +6,21 @@ export default class Contact extends Component {
     render() {
         return (
             <div className="contact-container">
-                <h1>CONTACT</h1>
-                <p>              <a href="mailto:lauradondiego@gmail.com">
+                <div className="headers">Contact Me</div>
+                <span> <a href="mailto:lauradondiego@gmail.com">
                     lauradondiego@gmail.com
                 </a>
 
-                </p>
-                <p>
+                </span>
+                <span>
                     (908) 907-0012
-            </p>
-                <p>
-                    {<a href="https://github.com/lauradondiego">Github</a>}
-                </p>
-                <p>
-                    {<a href="https://linkedin.com/in/lauradondiego">LinkedIn</a>}
-                </p>
+            </span>
+                <span>
+                    <a href="https://github.com/lauradondiego">Github</a>
+                </span>
+                <span>
+                    <a href="https://linkedin.com/in/lauradondiego">LinkedIn</a>
+                </span>
             </div>
         )
     }
