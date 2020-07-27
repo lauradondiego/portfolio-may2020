9ab3a02d9bde36e5d476a73453f85de3df1075f6
laura <lauradondiego@gmail.com>
2020-07-27 15:32:39 -0400

    added text into essays component

--- src/components/Essays.js
+++ src/components/Essays.js
@@ -9,7 +9,10 @@ export default class Essays extends Component {
                 <div className="essays-container">
 
                     <div className="headers">Essays</div>
-                    <a href="https://lauradondiego.substack.com/" target="_blank">Substack Essays</a>
+                    <span>Essays that may inspire you, by me.</span>
+                    <p>I explore a range of topics like streetwear, coding, gaming, and web development. </p>
+                    <p>I look forward to discussing how I transitioned into coding, created my clothing brand from the ground up, and expressing my thoughts within the gaming culture industry.  </p>
+                    <a href="https://lauradondiego.substack.com/" target="_blank">Substack</a>
                 </div>
             </>
         )
